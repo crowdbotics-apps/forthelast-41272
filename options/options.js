40c58fd33b6f11aff497625d6e1dd52ac281330f
Crowdbotics <team@crowdbotics.com>
2023-06-05 21:10:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "forthelast-41272",
+  url: "https://forthelast-41272.botics.co",
+  api: "https://forthelast-41272.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
